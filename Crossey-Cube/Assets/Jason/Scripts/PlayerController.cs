df50b4e384d94b555ba5403791b9040811d7c908
s241436 <s241436@students.aie.edu.au>
2025-09-24 19:55:45 +0930

    Worked a bit on flipping car bug
    Worked on road

--- Crossey-Cube/Assets/Jason/Scripts/PlayerController.cs
+++ Crossey-Cube/Assets/Jason/Scripts/PlayerController.cs
@@ -22,6 +22,7 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(-Vector3.forward * speed, ForceMode.Force);
         }
+
         
         if (Input.GetKey(KeyCode.LeftArrow))
         {
