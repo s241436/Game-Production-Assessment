e6920148a910f0225663f8d1502aec08cb626ac4
s241436 <s241436@students.aie.edu.au>
2025-11-05 19:56:28 +1030

    push

--- Crossey-Cube/Assets/James/PrototypeA/ObstaclesSpawn.cs
+++ Crossey-Cube/Assets/James/PrototypeA/ObstaclesSpawn.cs
@@ -13,29 +13,6 @@ public class ObstaclesSpawn : MonoBehaviour
     [SerializeField] GameObject groundPortal;
 
     int random = 0;
-/*
-    public float spawnDistance = 10f;
-    private float lastSpawnX;
 
-    void GroundSpawnObstacles(Collider other)
-    {
-        random = Random.Range(0, GroundObstacles.Length);
-        lastSpawnX += spawnDistance;
-        Vector3 spawnPos = new Vector3(lastSpawnX, 0, 0);
-
-
-        if (portalSpawned == false)
-        {
-            Instantiate(groundPortal, spawnPos, Quaternion.identity);
-            portalSpawned = true;
-        }
-        else
-        {
-
-            Instantiate(GroundObstacles[random], spawnPos, Quaternion.identity);
-        }
-
-
-    }*/
 
 }
